0503d5322e84a08718e891031e56af2c6845cdd5
ddx <13086352356@163.com>
2020-05-13 16:13:43 +0800

    server内容

---
+++ server/module/playlist_delete.js
@@ -0,0 +1,13 @@
+// 创建歌单
+
+module.exports = (query, request) => {
+  query.cookie.os = 'pc';
+  const data = {
+    ids: '[' + query.id + ']' 
+  };
+  return request('POST', `https://music.163.com/weapi/playlist/remove`, data, {
+    crypto: 'weapi',
+    cookie: query.cookie,
+    proxy: query.proxy
+  });
+};
