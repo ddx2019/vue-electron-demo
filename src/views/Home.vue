f237d2073cd7d7f8096bb7d25fb0ab60ca652028
ddx <13086352356@163.com>
2020-06-16 16:50:42 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,42 @@
 <template>
- <div class="home">
-   <nav-header></nav-header>
-   <div class="main-content">
-      <div class="left-container"> 
+  <div class="home">
+    <nav-header></nav-header>
+    <div class="main-content">
+      <div class="left-container">
         <left-nav></left-nav>
       </div>
       <div class="right-container">
         <router-view></router-view>
       </div>
-   </div>
- </div>
+    </div>
+  </div>
 </template>
 <script>
-import LeftNav from "@/components/nav/LeftNav.vue"
-import NavHeader from '@/components/header'
-  export default {
-    components:{
-      LeftNav,
-      NavHeader
-    }
+import LeftNav from "@/components/nav/LeftNav.vue";
+import NavHeader from "@/components/header";
+export default {
+  components: {
+    LeftNav,
+    NavHeader
   }
+};
 </script>
 <style lang="scss">
-  .home{
-    background: #faf9f9;
-    min-height:100vh;
-   .main-content{
-      display: flex;
-      justify-content: space-between;
-      .left-container{
-        min-width: 200px;
-      }
-      .right-container{
-        flex: auto;
-        max-width: 1100px;
-        margin: 0 auto;
-        padding:50px;
-      }
-   }
-
-  }  
+.home {
+  background: #faf9f9;
+  min-height: 100vh;
+  .main-content {
+    display: flex;
+    justify-content: space-between;
+    .left-container {
+      min-width: 200px;
+    }
+    .right-container {
+      flex: auto;
+      max-width: 1100px;
+      margin: 0 auto;
+      padding: 50px;
+    }
+  }
+}
 </style>
