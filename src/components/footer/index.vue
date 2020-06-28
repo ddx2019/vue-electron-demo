e0fc78bdfd332bd77cbf5a5f1ea482c0e4b16e53
ddx <13086352356@163.com>
2020-06-28 13:39:04 +0800

    在footer中测试font-awesome

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="footer">
-    <h1>top</h1>
-    <h2>bottom</h2>
+    <div class="currently-playing" />
+    <div class="play-bar" />
     <div class="el-icon-fa-wechat" />
   </div>
 </template>
 <style lang="scss">
 .footer{
-    width:100%;
+   width:100%;
    position:fixed;
    bottom:0;
    z-index:1000;
+   .currently-playing{
+       width: 200px;
+       border:1px solid #eee;
+       height: 60px;
+   }
 }
 </style>
