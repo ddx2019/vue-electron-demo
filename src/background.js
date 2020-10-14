9b8c1a596b66d87b06d51efa6cecd71d347dc0b3
ddx <13086352356@163.com>
2020-10-14 11:34:45 +0800

    去除eslint相关部分，change exec to spawn

--- src/background.js
+++ src/background.js
@@ -1,14 +1,22 @@
 /* eslint-disable no-undef */
-import { app, protocol, BrowserWindow, ipcMain, Tray, Menu, nativeImage } from 'electron'
 import {
-  createProtocol
-} from 'vue-cli-plugin-electron-builder/lib'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+  app,
+  protocol,
+  BrowserWindow,
+  ipcMain,
+  Tray,
+  Menu,
+  nativeImage
+} from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+const isDevelopment = process.env.NODE_ENV !== "production";
 
-let win
-let serverProcess = null
-let loading
-protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: true, standard: true }}])
+let win;
+let serverProcess = null;
+let loading;
+protocol.registerSchemesAsPrivileged([
+  { scheme: "app", privileges: { secure: true, standard: true } }
+]);
 
 function createWindow() {
   win = new BrowserWindow({
@@ -16,43 +24,45 @@ function createWindow() {
     height: 670,
     resizable: false, // 禁止改变主窗口尺寸,设置过后，win.isMaximized()总是返回false
     show: false, // 一开始是false,loadpage加载完毕的时候为true
+    // autoHideMenuBar: true,// 隐藏工具栏
     frame: false, // 关闭window自带的关闭等功能以及工具栏， 无边框窗口是不允许拖动的，可通过设置样式让其可拖动，样式见index.html中
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false // 允许跨域
     }
-  })
+  });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    setTimeout(() => { // 关闭loadpage的时候，将win, show true
-      loading.hide()
-      loading.close()
-      win.show()
-      win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    }, 5000)
+    setTimeout(() => {
+      // 关闭loadpage的时候，将win, show true
+      loading.hide();
+      loading.close();
+      win.show();
+      win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    }, 5000);
     // 开启渲染进程中的调试模式 if (!process.env.IS_TEST)
     // win.webContents.openDevTools() // 打开调试模式
-    win.webContents.closeDevTools() // 关闭调试模式
+    win.webContents.closeDevTools(); // 关闭调试模式
   } else {
     setTimeout(() => {
-      loading.hide()
-      loading.close()
-      win.show()
-      createProtocol('app')
-      win.loadURL('app://./index.html')
-    }, 5000)
-    win.webContents.closeDevTools() // 关闭调试模式
+      loading.hide();
+      loading.close();
+      win.show();
+      createProtocol("app");
+      win.loadURL("app://./index.html");
+    }, 5000);
+    win.webContents.closeDevTools(); // 关闭调试模式
   }
 
-  win.on('closed', () => {
-    win = null
-    app.quit()
-  })
+  win.on("closed", () => {
+    win = null;
+    app.quit();
+  });
 
-  win.on('close', function(_event) {
-    // 关闭前差一个弹窗，询问是否退出程序 *************；
-    stopServer() // 停止后台服务
-  })
+  win.on("close", function(_event) {
+    // ToDo 关闭前差一个弹窗，询问是否退出程序 *************；
+    stopServer(); // 停止后台服务
+  });
 }
 
 function showLoading(callback) {
@@ -62,176 +72,188 @@ function showLoading(callback) {
     width: 360,
     height: 360,
     resizable: false
-
-  })
-  loading.once('show', callback)
-  loading.loadURL(`file://${__static}/loadpage.html`) // 将loadpage作为加载页面，该页面存在项目的public文件夹下
-  loading.show()
+  });
+  loading.once("show", callback);
+  loading.loadURL(`file://${__static}/loadpage.html`); // 将loadpage作为加载页面，该页面存在项目的public文件夹下
+  loading.show();
 }
 
 function initApp() {
-  showLoading(createWindow)
+  showLoading(createWindow);
 }
 
 // 窗口 最小化
-ipcMain.on('window-min', function() { // 收到渲染进程的窗口最小化操作的通知，并调用窗口最小化函数，执行该操作
-  win.minimize()
-})
+ipcMain.on("window-min", function() {
+  // 收到渲染进程的窗口最小化操作的通知，并调用窗口最小化函数，执行该操作
+  win.minimize();
+});
 
 // 窗口 最大化、恢复
-ipcMain.on('window-max', function(_event, obj) {
+ipcMain.on("window-max", function(_event, obj) {
   // 通过 resizable: false 设置了禁止改变主窗口尺寸,设置过后，win.isMaximized()总是返回false,故此处不再用它判断。 win.restore();也失效
-  if (obj.winFlag) { // val为 true 则调用最大化函数
-    win.maximize()
+  if (obj.winFlag) {
+    // val为 true 则调用最大化函数
+    win.maximize();
   } else {
-    win.setContentSize(1122, 670) // 重新设置窗口客户端的宽高值（例如网页界面）,这里win.setSize(x,y)并不生效。
-    win.center() // 窗口居中
+    win.setContentSize(1122, 670); // 重新设置窗口客户端的宽高值（例如网页界面）,这里win.setSize(x,y)并不生效。
+    win.center(); // 窗口居中
   }
-})
+});
 
 // 关闭窗口
-ipcMain.on('window-close', function() {
-  win.close()
-})
+ipcMain.on("window-close", function() {
+  win.close();
+});
 
 // 所有窗口都关闭的时候触发，windows和Linux中，此时应用也应退出。
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (win === null) {
-    initApp()
+    initApp();
   }
-})
+});
 
 if (isDevelopment) {
   // electron完成初始化的时候触发
-  app.on('ready', async() => {
-    startServer() // 启动服务器
-    initApp()
-    implementSystemTray()
-  })
+  app.on("ready", async () => {
+    startServer(); // 启动服务器
+    initApp();
+    implementSystemTray();
+  });
 } else {
   // 限制只能开启一个应用(4.0以上版本)；
   // 当第二个实例被执行并且调用 app.requestSingleInstanceLock() 时，这个事件将在你的应用程序的首个实例中触发
-  const goTheLock = app.requestSingleInstanceLock()
+  const goTheLock = app.requestSingleInstanceLock();
   if (!goTheLock) {
-    app.quit()
+    app.quit();
   } else {
-    app.on('second-instance', (_event, _commandLine, _workingDirectory) => {
+    app.on("second-instance", (_event, _commandLine, _workingDirectory) => {
       if (win) {
-        if (win.isMinimized()) win.restore()
-        win.focus()
+        if (win.isMinimized()) win.restore();
+        win.focus();
       }
-    })
-    app.on('ready', async() => {
-      implementSystemTray()
-      startServer()
-      initApp()
-    })
+    });
+    app.on("ready", async () => {
+      implementSystemTray();
+      startServer();
+      initApp();
+    });
   }
 }
 
 // 系统托盘
-var appIcon = null
+var appIcon = null;
 
 function implementSystemTray() {
-  appIcon = new Tray('./public/favicon.ico') // 创建与image关联的图标
-  var contextMenu = Menu.buildFromTemplate([ // 创建菜单
+  appIcon = new Tray("./public/favicon.ico"); // 创建与image关联的图标
+  var contextMenu = Menu.buildFromTemplate([
+    // 创建菜单
     {
-      label: '上一首',
-      type: 'radio',
+      label: "上一首",
+      type: "radio",
       click: function() {
-        console.log('播放上一首歌曲')
+        console.log("播放上一首歌曲");
       }
     },
-    { label: '下一首', type: 'radio' },
-    { label: '打开桌面歌词', type: 'radio', checked: true },
-    { label: '退出', role: 'close', accelerator: 'CmdOrCtrl+Q' }
-  ])
-  appIcon.setToolTip('This is demo') // 鼠标放在该系统托盘图标上的提示信息
-  appIcon.setContextMenu(contextMenu)
+    { label: "下一首", type: "radio" },
+    { label: "打开桌面歌词", type: "radio", checked: true },
+    { label: "退出", role: "close", accelerator: "CmdOrCtrl+Q" }
+  ]);
+  appIcon.setToolTip("This is demo"); // 鼠标放在该系统托盘图标上的提示信息
+  appIcon.setContextMenu(contextMenu);
 
   // 消息通知，利用计时器，通过Image实例和空的Image实例 来实现图标闪动效果；
-  let timer = null
-  let msgFlag = false
-  ipcMain.on('have-message', (event, arg) => {
+  let timer = null;
+  let msgFlag = false;
+  ipcMain.on("have-message", (event, arg) => {
     timer = setInterval(() => {
-      msgFlag = !msgFlag
-      msgFlag ? appIcon.setImage(nativeImage.createEmpty()) : appIcon.setImage('./public/favicon.ico')
-      appIcon.setToolTip('您有一条新消息')
-    }, 500)
-  })
+      msgFlag = !msgFlag;
+      msgFlag
+        ? appIcon.setImage(nativeImage.createEmpty())
+        : appIcon.setImage("./public/favicon.ico");
+      appIcon.setToolTip("您有一条新消息");
+    }, 500);
+  });
 
-  appIcon.on('click', (_event, _bounds, _position) => { // 单击该系统托盘图标，显示隐藏窗口
+  appIcon.on("click", (_event, _bounds, _position) => {
+    // 单击该系统托盘图标，显示隐藏窗口
     if (win.isVisible()) {
-      win.hide()
+      win.hide();
     } else {
-      win.show()
-      appIcon.setImage('./public/favicon.ico') // 防止 win.show() 后系统托盘图标的 空白现象；
-      clearInterval(timer)
-      timer = null
-      msgFlag = false
+      win.show();
+      appIcon.setImage("./public/favicon.ico"); // 防止 win.show() 后系统托盘图标的 空白现象；
+      clearInterval(timer);
+      timer = null;
+      msgFlag = false;
     }
-  })
+  });
 }
 
 // 启动本项目中的服务器
 function startServer() {
-  let cmdStr = 'yarn install' // 要运行的命令
-  const serverPath = isDevelopment ? './server' : './server' // 注意开发环境和线上环境的路径不同；
+  let cmdStr = "yarn install"; // 要运行的命令
+  const serverPath = isDevelopment ? "./server" : "./server"; // 注意开发环境和线上环境的路径不同；
 
-  serverProcess = require('child_process').exec(cmdStr, { cwd: serverPath })
+  // exec 函数 第一个参数是要执行的命令，第二个函数是配置选项，第三个参数是回调函数，配置项中常用到 子进程的工作目录
+  serverProcess = require("child_process").spawn(cmdStr, {
+    cwd: serverPath,
+    shell: true
+  });
 
-  cmdStr = 'node app.js'
-  runExec(cmdStr)
+  cmdStr = "node app.js";
+  runExec(cmdStr);
 
   function runExec(cmdStr) {
-    // exec 函数 第一个参数是要执行的命令，第二个函数是配置选项，第三个参数是回调函数，配置项中常用到 子进程的工作目录
-    serverProcess = require('child_process').exec(cmdStr, { cwd: serverPath })
+    // spawn 函数 第一个参数是要执行的命令，第二个函数字符串参数的列表(省略后默认该值为空数组[])，第三个参数是配置选项，配置项中常用到 子进程的工作目录
+    serverProcess = require("child_process").spawn(cmdStr, {
+      cwd: serverPath,
+      shell: true
+    });
 
-    serverProcess.stdout.on('data', function(data) {
-      console.log('启动服务器成功 stdout:' + data) // 打印正常的后台可执行程序输出
-    })
-    serverProcess.stderr.on('data', function(data) {
-      console.log('stderr:' + data) // 打印错误的后台可执行程序输出
-    })
-    serverProcess.on('close', function(code) {
-      console.log('out code:' + code) // 退出之后的输出
-    })
+    serverProcess.stdout.on("data", function(data) {
+      console.log("启动服务器成功 stdout:" + data); // 打印正常的后台可执行程序输出
+    });
+    serverProcess.stderr.on("data", function(data) {
+      console.log("stderr:" + data); // 打印错误的后台可执行程序输出
+    });
+    serverProcess.on("close", function(code) {
+      console.log("out code:" + code); // 退出之后的输出
+    });
   }
 }
 
 // 关闭项目中的所有进程，主要是为了关闭刚刚启动的服务器进程。
 function stopServer() {
-  console.log('Kill server process.....')
-  const kill = require('tree-kill') //  tree-kill是一个插件，需要安装，在项目中已经用 yarn add tree-kill 命令安装。
+  console.log("Kill server process.....");
+  const kill = require("tree-kill"); //  tree-kill是一个插件，需要安装，在项目中已经用 yarn add tree-kill 命令安装。
   if (serverProcess) {
-    kill(serverProcess.pid, 'SIGTERM', function(_err) { // 终止进程树，杀死所有进程，包括根进程。
-      console.log('Killed')
-      serverProcess = null
-      win = null
-      app.quit()
-    })
+    kill(serverProcess.pid, "SIGTERM", function(_err) {
+      // 终止进程树，杀死所有进程，包括根进程。
+      console.log("Killed");
+      serverProcess = null;
+      win = null;
+      app.quit();
+    });
   }
 }
 
-app.stopServer = stopServer
+app.stopServer = stopServer;
 
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', data => {
-      if (data === 'graceful-exit') {
-        app.quit()
+  if (process.platform === "win32") {
+    process.on("message", data => {
+      if (data === "graceful-exit") {
+        app.quit();
       }
-    })
+    });
   } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
   }
 }
-
--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -4,15 +4,30 @@
     <el-row>
       <el-col :span="4">
         <span class="logo" @click="goHome">
-          <img class="no_drag" src="@/assets/image/wangyiyun.svg">
+          <img class="no_drag" src="@/assets/image/wangyiyun.svg" />
           <span class="no_drag">网易云音乐</span>
         </span>
       </el-col>
       <el-col :span="7">
         <div class="search_outside">
-          <span class="el-icon-arrow-left arrow no_drag" :class="{' arrow_active':arrow}" @click="handleClickArrow('left')" />
-          <span class="el-icon-arrow-right arrow no_drag" :class="{' arrow_active':!arrow}" @click="handleClickArrow('right')" />
-          <input ref="search" v-model="searchKey" class="search_input no_drag" type="text" @focus="handleFocus" @blur="handleBlur">
+          <span
+            class="el-icon-arrow-left arrow no_drag"
+            :class="{ ' arrow_active': arrow }"
+            @click="handleClickArrow('left')"
+          />
+          <span
+            class="el-icon-arrow-right arrow no_drag"
+            :class="{ ' arrow_active': !arrow }"
+            @click="handleClickArrow('right')"
+          />
+          <input
+            ref="search"
+            v-model="searchKey"
+            class="search_input no_drag"
+            type="text"
+            @focus="handleFocus"
+            @blur="handleBlur"
+          />
           <i class="el-icon-search no_drag" />
         </div>
       </el-col>
@@ -36,177 +51,178 @@
   </div>
 </template>
 <script>
-import { ipcRenderer, shell } from 'electron'
+import { ipcRenderer, shell } from "electron";
 export default {
   data() {
     return {
-      searchKey: '搜索音乐，视频，歌词，电台',
+      searchKey: "搜索音乐，视频，歌词，电台",
       arrow: true,
       winFlag: false
-    }
+    };
   },
   methods: {
     goHome() {
-    // location.reload();
-      this.$router.push({ path: '/' })
+      // location.reload();
+      this.$router.push({ path: "/" });
     },
     handleFocus() {
-      this.searchKey = ''
+      this.searchKey = "";
     },
     handleBlur() {
-      this.searchKey = '搜索音乐，视频，歌词，电台'
+      this.searchKey = "搜索音乐，视频，歌词，电台";
     },
     handleClickArrow(arg) {
-      if (arg === 'left') {
-        this.arrow = true
+      if (arg === "left") {
+        this.arrow = true;
       } else {
-        this.arrow = false
+        this.arrow = false;
       }
     },
     minimizeWin() {
-      ipcRenderer.send('window-min') // 通知主进程我要进行窗口最小化操作
+      ipcRenderer.send("window-min"); // 通知主进程我要进行窗口最小化操作
     },
     maximizeWin() {
-      this.winFlag = !this.winFlag
-      ipcRenderer.send('window-max', { winFlag: this.winFlag })
+      this.winFlag = !this.winFlag;
+      ipcRenderer.send("window-max", { winFlag: this.winFlag });
     },
     closeWin() {
-      ipcRenderer.send('window-close')
+      ipcRenderer.send("window-close");
     },
     openEx() {
-      shell.openExternal('https://www.baidu.com')
+      shell.openExternal("https://www.baidu.com");
     },
     handleClickSendMsg() {
-      ipcRenderer.send('have-message')
-      console.log('给主进程 发送消息')
+      ipcRenderer.send("have-message");
+      console.log("给主进程 发送消息");
       // 这里可放一个音频文件
       /* const audio=new Audio('audio的路径');
         audio.play(); */
     }
-
   }
-}
+};
 </script>
 <style lang="scss">
-   .header{
-      position: fixed;
-      width: 100%;
-      z-index: 1000;
-       .logo{
-           border:1px  solid transparent;
-           position: relative;
-           cursor: pointer;
-           display: block;
-           img{
-               height: 21px;
-               border-radius: 50%;
-               position: absolute;
-               top:0;
-               left:10px;
-           }
-           span{
-               display: inline-block;
-               position: absolute;
-               left: 35px;
-               font-size: 17px;
-               letter-spacing: 2px;
-           }
-       }
-      .el-row {
-          padding:12px 14px 12px 10px;
-          background: #c62f2f;
-          color: #fff;
-     }
-    .el-col {
-      border-radius: 4px;
+.header {
+  position: fixed;
+  width: 100%;
+  z-index: 1000;
+  .logo {
+    border: 1px solid transparent;
+    position: relative;
+    cursor: pointer;
+    display: block;
+    img {
+      height: 21px;
+      border-radius: 50%;
+      position: absolute;
+      top: 0;
+      left: 10px;
     }
-    .search_outside{
-      .arrow{
-        border: 1px solid #a82828;
-        display: inline-block;
-        width: 26px;
-        height: 21px;
-        line-height: 21px;
-        text-align: center;
-        font-size: 14px;
-        font-weight: 600;
-       color: rgb(177, 172, 172);
-      }
-      .arrow_active{
+    span {
+      display: inline-block;
+      position: absolute;
+      left: 35px;
+      font-size: 17px;
+      letter-spacing: 2px;
+    }
+  }
+  .el-row {
+    padding: 12px 14px 12px 10px;
+    background: #c62f2f;
+    color: #fff;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .search_outside {
+    .arrow {
+      border: 1px solid #a82828;
+      display: inline-block;
+      width: 26px;
+      height: 21px;
+      line-height: 21px;
+      text-align: center;
+      font-size: 14px;
+      font-weight: 600;
+      color: rgb(177, 172, 172);
+    }
+    .arrow_active {
+      color: #fff;
+    }
+    .search_input {
+      background: rgba(0, 0, 0, 0.1);
+      border: none;
+      height: 24px;
+      border-radius: 25px;
+      width: 210px;
+      margin-left: 10px;
+      color: rgb(184, 109, 109);
+      outline: none;
+      font-size: 13px;
+      padding-left: 10px;
+      display: inline-block;
+      &:focus {
         color: #fff;
       }
-     .search_input{
-        background: rgba(0,0,0,0.1);
-        border: none;
-        height: 24px;
-        border-radius: 25px;
-        width: 210px;
-        margin-left: 10px;
-        color: rgb(184, 109, 109);
-        outline: none;
-        font-size: 13px;
-        padding-left: 10px;
-        display: inline-block;
-        &:focus{
-          color: #fff;
-        }
-      }
-      i{
-        margin-left: -28px;
-        font-size: 13px;
-        cursor: pointer;
-      }
-
     }
-    .personal_outside{
-      text-align: right;
-      color: rgb(221, 220, 220);
-      i{
-        display: inline-block;
-        width: 24px;
-        height: 24px;
-        line-height: 24px;
-        border-radius: 100%;
-        background: #eee;
-        text-align: center;
-        vertical-align: middle;
-        color: rgb(204, 204, 204);
-        cursor: pointer;
-
-      }
-      span{
-        font-size: 16px;
-        margin-left: 20px;
-        cursor: pointer;
-      }
-      .el-icon-caret-bottom{
-        margin-left: 5px;
-      }
-      .icon-max-size{
-        display: inline-block;
-        height: 10px;
-        width: 12px;
-        border-radius: 2px;
-        border: 1px solid  rgb(177, 172, 172);
-      }
-      .text_loginstatus,.text_VIP{
-        font-size:12px;
-        display:inline-block;
-        margin-left: 6px;
-      }
-      .text_VIP{
-        margin-left: 10px;
-      }
-      .el-icon-close{
-        font-size: 18px;
-      }
-      strong{
-        margin-left: 18px;
-        color:rgb(167, 70, 70);
-      }
-      .el-icon-edit-outline,.el-icon-minus,.el-icon-full-screen,.el-icon-close{
-        margin-left: 8px;
-      }
+    i {
+      margin-left: -28px;
+      font-size: 13px;
+      cursor: pointer;
     }
+  }
+  .personal_outside {
+    text-align: right;
+    color: rgb(221, 220, 220);
+    i {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+      line-height: 24px;
+      border-radius: 100%;
+      background: #eee;
+      text-align: center;
+      vertical-align: middle;
+      color: rgb(204, 204, 204);
+      cursor: pointer;
+    }
+    span {
+      font-size: 16px;
+      margin-left: 20px;
+      cursor: pointer;
+    }
+    .el-icon-caret-bottom {
+      margin-left: 5px;
+    }
+    .icon-max-size {
+      display: inline-block;
+      height: 10px;
+      width: 12px;
+      border-radius: 2px;
+      border: 1px solid rgb(177, 172, 172);
+    }
+    .text_loginstatus,
+    .text_VIP {
+      font-size: 12px;
+      display: inline-block;
+      margin-left: 6px;
+    }
+    .text_VIP {
+      margin-left: 10px;
+    }
+    .el-icon-close {
+      font-size: 18px;
+    }
+    strong {
+      margin-left: 18px;
+      color: rgb(167, 70, 70);
+    }
+    .el-icon-edit-outline,
+    .el-icon-minus,
+    .el-icon-full-screen,
+    .el-icon-close {
+      margin-left: 8px;
+    }
+  }
 }
 </style>
